f7d001f64110a5df5e39fa912d10e5ead3ded807
klittle <klittle@eecs.wsu.edu>
2018-11-04 13:19:41 -0800

    program works for arbitrary # nodes, arbitrary modulo, and arbitrary offset

--- project.py
+++ project.py
@@ -67,42 +67,77 @@ def numSatisfiesExpr(num, expression, bits_needed):
         if debug: print("==> NO -- {0} ({1}) doesn't satisify expression.".format(num, binNumStr))
         result = False
     return result
-        
+
+def compose(H, F):
+    """Compose operator discussed in class
+    """
+    bitsPerNode = int(H.degree/2)
+    Z = exprvars('Z', bitsPerNode)
+    H = H.compose({H.inputs[i+bitsPerNode]: Z[i] for i in range(bitsPerNode)})
+    F = F.compose({F.inputs[i]: Z[i] for i in range(bitsPerNode)})
+    result = H & F
+
+    # Now, check for existence... 
+    # i.e. there exists z1, z2, ..., zk such that H(x1, x2, ..., xk, z1, ..., zk) & F(z1, ..., zk, y1, .... yk)
+    result = result.smoothing(Z)
+    return result
+
+def computeTransitiveClosure(boolExpr):
+    # Initialize _H as initial boolean expression
+    _H = boolExpr
+    H = Or()        # some random Expression not equal to _H
+    edgesToReach = 2; max_iterations = 5
+    canReachInXStepsExpr = {1:boolExpr}
+    while _H.equivalent(H) == False and edgesToReach < max_iterations:
+        H = _H
+        _H = (compose(_H, H) | _H).simplify()
+        canReachInXStepsExpr[edgesToReach] = _H
+        edgesToReach += 1
+    return canReachInXStepsExpr
+
+def canReach(a, b, canReachInXStepsExpr, modulo, bits_needed):
+    """Determines whether node a can reach node b. Returns number of edges to reach b if possible;
+    otherwise, returns None
+    """
+    assert isinstance(a, int) and isinstance(b, int)
+    for key in canReachInXStepsExpr.keys():
+        expr = canReachInXStepsExpr[key]
+        edgeNumber = (uint2exprs(a%modulo, length=bits_needed) + uint2exprs(b%modulo, length=bits_needed)).to_uint()
+        if numSatisfiesExpr(edgeNumber, expr, bits_needed*2) == True:
+            return key
+    else:
+        return None
 
 def main():
     # Step 1. Build Boolean Decision Diagram (BDD) denoted as F.
-    """
-    First build a Boolean expression; then convert it to BDD.
-    The expression must accept 10 bit variables. If those 10 bits represent an edge, the expression will return true (1).
-
-    I can then test my answer by calling numSatisfiesExpr(  (expr(i, length=5) + expr(j, length=5)).to_uint(), expr  )
-    """
+    # Vary the following parameters to observe different program behavior.
     nodes = 32
     modulo = 32
     offset = 0
     bits_needed = uint2exprs(nodes + offset - 1).size
+
+    # Build expr that accepts 10 1-bit variables. If those 10 bits represent an edge, the expression will return true
     boolExpr = generateEdgeExpr(modulo, nodes, offset).simplify()
+
+    # Test code to ensure all edges are correct.
     for i in range(offset, nodes+offset):
         for j in range(offset, nodes+offset):
             if (i+3)%modulo == j%modulo or (i+7)%modulo == j%modulo:
-                edgeNumber = (uint2exprs(i%32, length=bits_needed) + uint2exprs(j%32, length=bits_needed)).to_uint()
+                edgeNumber = (uint2exprs(i%modulo, length=bits_needed) + uint2exprs(j%modulo, length=bits_needed)).to_uint()
                 assert numSatisfiesExpr(edgeNumber, boolExpr, bits_needed*2) == True
                 print("There is an edge from {0} to {1}.".format(i,j))
     F = expr2bdd(boolExpr)
     graph_BDD(F, "img/F.gv")
 
-
     # Step 2. Compute the transitive closure of F, denoted as transitiveF
-    """
-    ...
-    """
+    canReachInXStepsExpr = computeTransitiveClosure(boolExpr)
 
     # Step 3. Can node i reach node j in one or more steps?
-    #         Also, from BDD transitiveF, is some property P satisfied?
-    """
-    ...
-    """
-
+    for i in range(offset, nodes+offset):
+        for j in range(offset, nodes+offset):
+            result = canReach(i, j, canReachInXStepsExpr, modulo, bits_needed)
+            if result is not None:
+                print("Node {0} can reach node {1} in {2} edge(s)".format(i,j,result))
 
 if __name__ == '__main__':
     main()
